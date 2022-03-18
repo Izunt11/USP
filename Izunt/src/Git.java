93dc48c540d63e24195fa3d6de001989fba8f932
Yoan <Yoan@DESKTOP-JHPG4OQ>
2022-03-18 15:39:01 +0200

    some change

--- Izunt/src/Git.java
+++ Izunt/src/Git.java
@@ -10,5 +10,7 @@ private static void displayInfo()
 System.out.println("USP 2018 Example project");
 System.out.println ("6itHub integration ");
 System.out.println ("Just new line");
+
+System.out.println("what about the conflict");
 }
 }
