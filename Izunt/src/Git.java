474497535196ba6b3647fd171c0c10fcc497c61b
lenovo <lenovo@DESKTOP-OLO634Q>
2022-03-18 15:38:44 +0200

    System.out.println

--- Izunt/src/Git.java
+++ Izunt/src/Git.java
@@ -1,4 +1,14 @@
-
-public class Git {
-
+public class Git 
+{
+	public static void main (String[] args)
+	{
+	displayInfo();
+	}
+	
+	private static void displayInfo()
+	{
+	System.out.println("USP 2018 Example project");
+	System.out.println ("6itHub integration ");
+	Systen.out.println ("Just new line");
+	}
 }
